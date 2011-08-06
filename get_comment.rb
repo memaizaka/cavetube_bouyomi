de6654ae13076d25695f85825cb4181576edab63
Memaizaka <memaizaka.net@gmail.com>
2011-08-06 09:56:24 +0900

    引数チェック

--- get_comment.rb
+++ get_comment.rb
@@ -6,34 +6,40 @@ require 'net/http'
 require 'json'
 require_relative 'bouyomi_socket'
 
-query = "stream_name=#{ARGV[0]}"
-from = ARGV[1] ? "&comment_num=#{ARGV[1]}" : ''
-single = ARGV[2] ?  "&single=#{ARGV[2]}" : ''
-query << from << single
-
-comment = Struct.new("Comment", :name, :message, :time, :num)
-
-comments = []
-Net::HTTP.version_1_2
-Net::HTTP.start 'gae.cavelis.net' do |http|
-  res = http.post('/viewedit/getcomment', query)
-  JSON.parse(res.body).each do |k,v|
-    case k
-      when /num_(\d+)/
-        comments << comment.new(v['name'], v['message'], v['time'], v['comment_num'])
-      else
-        puts "#{k} => #{v}"
+if ARGV.size < 1
+  $stderr.puts "少なくともstream_nameを引数に入れてください"
+  $stderr.puts "stream_name startnum get_single_comment?"
+  $stderr.puts "ex: AAAAAAAAAAAAAAAAAAAAAAAAA 1 true"
+else
+  query = "stream_name=#{ARGV[0]}"
+  from = "&comment_num=%s" % (ARGV[1] || '1')
+  single = ARGV[2] ?  "&single=#{ARGV[2]}" : ''
+  query << from << single
+  
+  comment = Struct.new("Comment", :name, :message, :time, :num)
+  
+  comments = []
+  Net::HTTP.version_1_2
+  Net::HTTP.start 'gae.cavelis.net' do |http|
+    res = http.post('/viewedit/getcomment', query)
+    JSON.parse(res.body).each do |k,v|
+      case k
+        when /num_(\d+)/
+          comments << comment.new(v['name'], v['message'], v['time'], v['comment_num'])
+        else
+          puts "#{k} => #{v}"
+      end
     end
   end
-end
-
-comments.sort_by! {|c| c.num}
-
-b = BouyomiSocket.new
-
-comments.each do |c|
-  m = c.num.to_s + "番\n"
-  m << c.name << "さん\n" unless c.name == ''
-  m << c.message
-  b.talk(m)
+  
+  comments.sort_by! {|c| c.num}
+  
+  b = BouyomiSocket.new
+  
+  comments.each do |c|
+    m = c.num.to_s + "番\n"
+    m << c.name << "さん\n" unless c.name == ''
+    m << c.message
+    b.talk(m)
+  end
 end
